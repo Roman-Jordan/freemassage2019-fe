f4605e30791f88d4dc02ff996f575bd644827cda
roman-jordan <rjordan1352@gmail.com>
2019-11-25 08:15:34 -0800

    yo

--- newContact.php
+++ newContact.php
@@ -5,8 +5,8 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require_once "./PHPMailer-master/src/PHPMailer.php";
-require_once "./PHPMailer-master/src/Exception.php";
+require_once "./mailer/PHPMailer-master/src/PHPMailer.php";
+require_once "./mailer/PHPMailer-master/src/Exception.php";
 
 
 if(isset($_POST)){
