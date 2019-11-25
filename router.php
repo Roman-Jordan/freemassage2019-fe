d3d4b4fd99d373657de409182f205b3909407c68
roman-jordan <rjordan1352@gmail.com>
2019-11-24 21:54:28 -0800

    yo

--- router.php
+++ router.php
@@ -37,7 +37,7 @@ if (isset($_SERVER['QUERY_STRING']) && isset($_GET['email']) && isset($_GET['cam
         <script>
             const objOne = {
                 email: '<?php echo $_GET['email'] ?>',
-                campaign: '<?php echo $_GET['camp'] ?>'
+                campaign: <?php echo $_GET['camp'] ?>
             }
             axios.post('https://freemassage.herokuapp.com/campaign_opened', objOne)
                 .then(res => console.log('response from mailer', res))
